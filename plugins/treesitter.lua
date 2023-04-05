37bf403b7d0e6830ca71f436f6416cdac5aec92c
Himujjal Upadhyaya <himu@tuta.io>
2023-04-05 15:50:14 +0530

    First user config

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "javascript", "typescript", "zig", "json", "json5", "svelte", "scss", "jsdoc", "json5", "html", "css"},
   },
 }
